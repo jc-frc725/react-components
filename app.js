2447add989820972cf81506bd89b87c945f38463
Justin Chow <hrll71990@gmail.com>
2020-12-07 10:57:38 -0800

    created GroceryList component and used ReactDOM.render to render to index.html

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // TODO
+
+// create a GroceryList component
+// var GroceryList = () => { return
+//     <ul>
+//       <li>Beer</li>
+//       <li>Noodles</li>
+//     </ul>
+// };
+
+// note: ES6 syntax, withou {} implicitly return
+var GroceryList = () => (
+  <ul>
+    <li>Beer</li>
+    <li>Noodles</li>
+  </ul>
+);
+
+// render GroceryList onto div tag in index.html with id of app
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
