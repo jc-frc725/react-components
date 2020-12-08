f20f806527e39dafd3cda82640278228d4458f26
Justin Chow <hrll71990@gmail.com>
2020-12-07 20:31:22 -0800

    implemented onClick functions on all GroceryListItem component

--- app.js
+++ app.js
@@ -3,29 +3,46 @@
 // note: ES6 syntax, withou {} implicitly return
 
 // props would become a list of GroceryListItems
-var GroceryList = (props) => (
+var GroceryList = (props) => {
   // props at [i] is a GroceryListItem, add to ul element
-  <div>
-    <ListTitle />
-    <ul>
-      {props.groceryItems[0]}
-      {props.groceryItems[1]}
-      {props.groceryItems[2]}
-      {props.groceryItems[3]}
-    </ul>
-  </div>
-);
+
+  // new feature: mouseover/clicking an item in the GroceryList does something
+  // This click function will be called when the first `<li>` below is clicked on
+
+  // We want to define it where it has access to `props`
+  // var onListItemClick = (event) => {
+  //   console.log('yea boi');
+  // };
+
+  return (
+    <div>
+      <ListTitle />
+      <ul>
+        {props.groceryitems[0]}
+        {props.groceryitems[1]}
+        {props.groceryitems[2]}
+        {props.groceryitems[3]}
+      </ul>
+    </div>
+  );
+};
 
 var ListTitle = () => (
   <h2>Stuff to Buy</h2>
 );
 
 // make a reusable GroceryListItem component that dynamically renders given grocery item
-var GroceryListItem = (props) => (
+var GroceryListItem = (grocery) => {
   // props needs to contain a dynamic 'name' to each GListItem
   // could be props.name = 'name of item', still within a li element
-  <li>{props.name}</li>
-);
+  var onListItemClick = (event) => {
+    console.log('yea boi');
+  };
+  return (
+    <li onClick={onListItemClick}>{grocery.name}</li>
+  );
+  
+};
 
 // make an array of GroceryListItems
 var groceryItems = [
@@ -37,4 +54,4 @@ var groceryItems = [
 ];
 
 // render GroceryList onto div tag in index.html with id of app
-ReactDOM.render(<GroceryList groceryItems={groceryItems}/>, document.getElementById("app"));
+ReactDOM.render(<GroceryList groceryitems={groceryItems}/>, document.getElementById("app"));
